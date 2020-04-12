d4cb5242b42d192efcbc71041662b1969d750cb2
Shiao-li Green <shiao-li@minerva.kgi.edu>
2020-04-12 13:31:01 +0100

    fix host and try to fix connection

--- tests/script.py
+++ tests/script.py
@@ -6,28 +6,28 @@ from sqlalchemy import create_engine
 class tests(unittest.TestCase):
 
     def setUp(self):
-        self.engine = create_engine("postgresql:///cs162_user:cs162_password@localhost:5432/cs162")
-        self.engine.connect()
+        self.engine = create_engine("postgresql:///cs162_user:cs162_password@192.168.99.100:5432/cs162")
+        self.connection = self.engine.connect()
 
     def tearDown(self):
         self.engine.close()
 
     def test_good_expression(self):
-        r = requests.post("http://localhost:5000/add", data={"expression": "42+21"})
+        r = requests.post("http://192.168.99.100:5000/add", data={"expression": "42+21"})
         self.assertEqual(r.status_code, 200)
 
     def test_database(self):
-        r = requests.post("http://localhost:5000/add", data={"expression": "42+21"})
+        r = requests.post("http://192.168.99.100:5000/add", data={"expression": "42+21"})
         query = self.connection.execute("SELECT * FROM Expression WHERE text='42+21'")
         rows = query.fetchall()
         self.assertEqual(len(rows), 1)
 
     def test_bad_expression(self):
-        r = requests.post("http://localhost:5000/add", data={"expression": "42+"})
+        r = requests.post("http://192.168.99.100:5000/add", data={"expression": "42+"})
         self.assertNotEqual(r.status_code, 200)
 
     def test_bad_db(self):
-        r = requests.post("http://localhost:5000/add", data={"expression": "42+"})
+        r = requests.post("http://192.168.99.100:5000/add", data={"expression": "42+"})
         self.assertNotEqual(r.status_code, 200)
         query = self.connection.execute("SELECT * FROM Expression WHERE text='42+'")
         rows = query.fetchall()
