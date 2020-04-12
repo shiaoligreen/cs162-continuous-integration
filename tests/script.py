04ce8f5811c3664f59e6d52eafa8ce6e588b2eef
Shiao-li Green <shiao-li@minerva.kgi.edu>
2020-04-12 13:48:51 +0100

    Update script.py
    change connection to engine

--- tests/script.py
+++ tests/script.py
@@ -5,12 +5,12 @@ from sqlalchemy import create_engine
 
 class tests(unittest.TestCase):
 
-    def setUp(self):
-        self.engine = create_engine("postgresql:///cs162_user:cs162_password@192.168.99.100:5432/cs162")
-        self.connection = self.engine.connect()
-
-    def tearDown(self):
-        self.engine.close()
+    # def setUp(self):
+    #     self.engine = create_engine("postgresql:///cs162_user:cs162_password@192.168.99.100:5432/cs162", echo=True)
+    #     self.connection = self.engine.connect()
+    #
+    # def tearDown(self):
+    #     self.engine.close()
 
     def test_good_expression(self):
         r = requests.post("http://192.168.99.100:5000/add", data={"expression": "42+21"})
@@ -18,7 +18,9 @@ class tests(unittest.TestCase):
 
     def test_database(self):
         r = requests.post("http://192.168.99.100:5000/add", data={"expression": "42+21"})
-        query = self.connection.execute("SELECT * FROM Expression WHERE text='42+21'")
+        engine = create_engine("postgresql:///cs162_user:cs162_password@192.168.99.100:5432/cs162", echo=True)
+        connection = engine.connect()
+        query = connection.execute("SELECT * FROM Expression WHERE text='42+21'")
         rows = query.fetchall()
         self.assertEqual(len(rows), 1)
 
@@ -28,8 +30,9 @@ class tests(unittest.TestCase):
 
     def test_bad_db(self):
         r = requests.post("http://192.168.99.100:5000/add", data={"expression": "42+"})
-        self.assertNotEqual(r.status_code, 200)
-        query = self.connection.execute("SELECT * FROM Expression WHERE text='42+'")
+        engine = create_engine("postgresql:///cs162_user:cs162_password@192.168.99.100:5432/cs162", echo=True)
+        connection = engine.connect()
+        query = connection.execute("SELECT * FROM Expression WHERE text='42+'")
         rows = query.fetchall()
         self.assertEqual(len(rows), 0)
 
